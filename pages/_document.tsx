92737d7554ea39a8fbcde049abbcc964e6824706
s.feli <s.feli@DESKTOP-8HQAAOS>
2023-12-01 22:31:11 +0330

    set initial direction be rtl

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { i18n } from 'next-i18next';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang={i18n?.language === "en" ? "en" :"fa"} dir={i18n?.language === "en" ? "ltr" :"rtl"}>
       <Head />
       <body className="bg-body-background" >
         <Main />
