c75eb521803c0df05c2fb60a4c52cb9f57d00018
s.feli <samran.f.m@gmail.com>
2024-01-09 17:59:25 +0330

    sort hotels based on availability

--- modules/domesticHotel/components/hotelsList/index.tsx
+++ modules/domesticHotel/components/hotelsList/index.tsx
@@ -16,23 +16,40 @@ const HotelsList: React.FC<Props> = props => {
     const firstItemIndex = (currentPage - 1) * 20;
     const lastItem = currentPage * 20;
 
+    const filteredHotels = [...hotels];
+
+    filteredHotels?.sort((b: PricedHotelItem, a: PricedHotelItem) => {
+
+        if (a.priceInfo === "loading" || b.priceInfo === 'loading') {
+            return 1;  // or -1?
+        } else if (a.priceInfo === "notPriced" && b.priceInfo !== "notPriced") {
+            return -1;
+        } else if (a.priceInfo !== "notPriced" && b.priceInfo === "notPriced") {
+            return 1;
+        } else if (a.priceInfo === "need-to-inquire") {
+            return -1;
+        } else {
+            return 1
+        }
+    });
+
     return (
         <>
 
             <Pagination
                 onChange={(page: number) => { setCurrentPage(page) }}
                 itemsPerPage={20}
-                totalItems={hotels?.length || 0}
+                totalItems={filteredHotels?.length || 0}
                 currentPage={currentPage}
                 wrapperClassName="mb-4"
             />
 
-            {hotels.slice(firstItemIndex, lastItem).map(hotel => <HotelListItem key={hotel.HotelId} hotel={hotel} />)}
+            {filteredHotels.slice(firstItemIndex, lastItem).map(hotel => <HotelListItem key={hotel.HotelId} hotel={hotel} />)}
 
             <Pagination
                 onChange={(page: number) => { setCurrentPage(page) }}
                 itemsPerPage={20}
-                totalItems={hotels?.length || 0}
+                totalItems={filteredHotels?.length || 0}
                 currentPage={currentPage}
             />
 
