3a08393ed082ff02ec78e3ade049e52b41c66017
Ali Adibpour <aliadp0101@gmail.com>
2024-01-29 10:55:16 +0330

    add Home icon to breadcrump

--- modules/blogs/components/template/BreadCrumpt.tsx
+++ modules/blogs/components/template/BreadCrumpt.tsx
@@ -1,3 +1,4 @@
+import { Home } from "@/modules/shared/components/ui/icons";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -7,8 +8,8 @@ const NavbarBlog: NextPage<any> = ({data , category}) => {
     return (
         <div
             className={`w-full flex max-w-container ml-auto mr-auto text-xs space-x-2 rtl:pr-5 ltr:pl-5 rtl:max-sm:pr-3 `}>
-            <Link href='/' className="text-gray-400">
-                Home
+            <Link href='/'>
+                <Home className="w-5 opacity-30"/>
             </Link><p className="text-gray-400">/</p>
             <Link href='/blog' className={`${data && 'text-gray-400'}`}>
                 بلاگ
