56a94361bae2bea26a5cb98c50c817372f498461
s.feli <samran.f.m@gmail.com>
2024-03-17 19:52:05 +0330

    cip price block

--- modules/shared/components/ui/Accordion.tsx
+++ modules/shared/components/ui/Accordion.tsx
@@ -4,6 +4,7 @@ type Props = {
     content: React.ReactNode;
     title: React.ReactNode;
     WrapperClassName?: string;
+    type2?:boolean;
 }
 
 const Accordion: React.FC<Props> = props => {
@@ -11,6 +12,8 @@ const Accordion: React.FC<Props> = props => {
     const contentRef = useRef<HTMLDivElement>(null);
 
     const [open, setOpen] = useState<boolean>(false);
+
+    const {content, title, WrapperClassName, type2} = props;
 
     const toggle = () => { setOpen(prevState => !prevState) }
 
@@ -23,16 +26,22 @@ const Accordion: React.FC<Props> = props => {
     }, [open]);
 
     return (
-        <div className={`border border-neutral-200 rounded-lg text-sm sm:text-base text-neutral-700 ${props.WrapperClassName || ""}`}>
+        <div className={`${type2 ? "":"border border-neutral-200 rounded-lg"} text-sm sm:text-base text-neutral-700 ${WrapperClassName || ""}`}>
+            {type2 ? (
+                <div onClick={toggle} className={`py-1 relative select-none cursor-pointer after:block after:w-2 after:h-2 after:border-b-2 after:border-l-2 after:top-1/2 after:-mt-1 after:border-neutral-400 after:self-center after:transition-all after:absolute after:rtl:right-0 after:ltr:left-0 rtl:pr-4 ltr:pl-4 ${open?"after:rotate-135":"after:-rotate-45"}`}>
+                {title}
+                </div>
+            ):(
             <div onClick={toggle} className={`leading-4 relative select-none cursor-pointer p-3 sm:p-5 after:block after:w-2 after:h-2 after:border-b-2 after:border-l-2 after:top-1/2 after:-mt-1 after:border-neutral-400 after:self-center after:transition-all after:absolute after:rtl:left-5 after:ltr:right-5 rtl:pl-12 ltr:pr-12 ${open?"after:rotate-135":"after:-rotate-45"}`}>
-                {props.title}
+                {title}
             </div>
+            )}
             <div
                 ref={contentRef}
                 className="overflow-hidden transition-all duration-300"
             >
-                <div className="bg-neutral-100 p-3 m-3 sm:p-5 sm:m-5 mt-0 sm:mt-0 text-justify">
-                    {props.content}
+                <div className={`${type2?"rtl:pr-5 ltr:pl-5 ":"bg-neutral-100 p-3 m-3 sm:p-5 sm:m-5 mt-0 sm:mt-0 text-justify"}`}>
+                    {content}
                 </div>
             </div>
         </div>
